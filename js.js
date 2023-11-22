44a4c2b084728fb3833c61dec32ac3ceaa769b7e
matheus <matheus.mariano9@fatec.sp.gov.br>
2023-11-22 13:51:01 -0300

    Alterações funções js

--- js.js
+++ js.js
@@ -3,9 +3,12 @@ function Entra(){
     window.open("cadastro.html")
 }
 function Banco(){
-    const banco = []
-    localStorage.setItem("banco", JSON.stringify(banco))
+    if (localStorage.getItem("banco") === null) {
+        const banco = [];
+        localStorage.setItem("banco", JSON.stringify(banco));
+    }
 }
+
 function Cadastro(){
     const banco = JSON.parse(localStorage.getItem("banco"))
     let user = document.querySelector("#user").value
@@ -15,26 +18,33 @@ function Cadastro(){
     if(pass == conf_pass){
         banco.push(dados)
         localStorage.setItem("banco", JSON.stringify(banco))
-        window.location.replace("login.html")
+        window.location.href = "login.html"
     }
     else{
         alert("As senhas não são iguais!")
         window.location.reload()
     }
 }
+
 function Login(){
     const banco = JSON.parse(localStorage.getItem("banco"))
     let user = document.querySelector("#user").value
     let pass = document.querySelector("#pass").value
-    if(user == banco[0].user && pass == banco[0].pass){
-        sessionStorage.setItem("logado", JSON.stringify(banco))
-        window.open("index.html")
+
+    let usuariologado = false
+
+    for(i = 0; i < banco.length; i++){
+        if(user == banco[i].user && pass == banco[i].pass){
+            usuariologado = true
+            sessionStorage.setItem("logado", JSON.stringify(banco))
+            window.location.href="index.html"
+            break;
+        
+        }
     }
-    else{
+    if(!usuariologado){
         alert("Usuário ou senha errados")
         window.location.reload()
     }
-}
-function Logado(){
-
+      
 }
