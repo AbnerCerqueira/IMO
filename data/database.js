dfca07c8960e447fc87b46d36de98b517a654435
AbnerCerqueira <abner.cerqueira@fate.sp.gov.br>
2024-05-23 11:00:57 -0300

    fixed: conexão com o banco

--- data/database.js
+++ data/database.js
@@ -3,10 +3,20 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-
 const con = mysql.createConnection({
+    port: process.env.MYSQL_PORT || '3306',
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DATABASE || 'imo'
 })
+
+con.connect((err) => {
+    if (err) {
+        console.log('Erro ao conectar com o banco\n' + err)
+        return
+    }
+    console.log('Conectado ao banco de dados')
+})
+
+module.exports = con
