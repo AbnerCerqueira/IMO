36820c4fbbab6a7a890954a3f53af4e67e2ff0b4
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-05-28 11:58:43 -0300

    style: organização diretorios

--- public/js/header.js
+++ public/js/header.js
@@ -3,11 +3,11 @@ function teste(){
     botao = document.getElementById('teste')
 
     if (verificar == 0) {
-        botao.src = '/img/headerlogoazul.svg'
+        botao.src = '/img/header/headerlogoazul.svg'
         verificar = 1
         
     } else {
-        botao.src = '/img/headerlogoinicial.svg'
+        botao.src = '/img/header/headerlogoinicial.svg'
         verificar = 0
     }
 } 
